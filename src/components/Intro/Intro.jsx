b92f1bcc085d3478ceea08fa1ab42582564dcf30
Nahom Assefa <nzenebe@gmail.com>
2022-02-28 22:21:33 -0600

    set up

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Intro() {
+  return <div className="i">
+      <div className="i-left"></div>
+      <div className="i-left wrapper"></div>
+      <div className="i-right"></div>
+      </div>;
+}
+
+export default Intro;
