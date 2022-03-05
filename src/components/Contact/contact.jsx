725806f9fa30ecf371e7d4e18fe4f447f8378c70
Nahom Assefa <nzenebe@gmail.com>
2022-03-05 16:36:08 -0600

    final push before readME

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -3,7 +3,7 @@ import { validateEmail } from "../../utils/helpers";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
 import { faSmile } from "@fortawesome/free-solid-svg-icons";
 
 function Contact() {
@@ -42,100 +42,90 @@ function Contact() {
         setErrorMessage("");
       }
     }
-    console.log('touchdown');
+    console.log("touchdown");
     setFormState({ ...formState, [e.target.name]: e.target.value });
-    console.log('put a string on it and see', formState);
+    console.log("put a string on it and see", formState);
   }
 
   return (
     <div className="contact">
-      <div className="c-bg"></div>
-      <h1 className="c-title"> Let's Work Together</h1>
-      <div className="c-wrapper">
-        <div className="c-left">
-          <h3>
-            Contact Info <FontAwesomeIcon icon={faSmile}></FontAwesomeIcon>
-          </h3>
-          <div className="c-info">
-            <div className="c-info-item">
-              <div className="c-icon">
-                <FontAwesomeIcon icon={faPhone}></FontAwesomeIcon>
-              </div>
-              +1 952 451 5678
-            </div>
+      <div className="c-info">
+        <h3>
+          Contact Info 
+        </h3>
+        <div className="c-group-items">
+          <div className="c-info-item">
+            <span className="c-icon">
+            <a href="tel:952.451.5678" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faPhone}></FontAwesomeIcon></a>
+            </span>
+            +1 952 451 5678
+          </div>
 
-            <div className="c-info-item">
-              <div className="c-icon">
-                <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
-              </div>
-              nahomassefa95@gmail.com
-            </div>
+          <div className="c-info-item">
+            <span className="c-icon">
+              <a href="mailto:nahomassefa95@gmail.com" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon></a> 
+            </span>
+            nahomassefa95@gmail.com
+          </div>
 
-            <div className="c-info-item">
-              <div className="c-icon">
-                <FontAwesomeIcon icon={faHouse}></FontAwesomeIcon>
-              </div>
-              5377 143rd Street West Savage, MN, 55378
-            </div>
+          <div className="c-info-item">
+            <span className="c-icon">
+              <a href="https://www.linkedin.com/in/nahom-assefa-163ba414b/" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faLink}></FontAwesomeIcon></a>
+            </span>
+            bit.ly/NahomAssefa
           </div>
         </div>
-        <div className="c-right">
-          <p className="c-desc">
-            <b> Leave a message? </b> Get in touch. Always interested if the
-            right project comes along!
-          </p>
-          {/* <form>
-            <input type="text" name="name" placeholder='name' />
-            <input type="text" name="subject" placeholder='subject' />
-            <input type="text" name="email" placeholder='email' />
-            <textarea rows="5" placeholder='Message' name="message" />
-            <button>Submit</button>
-        </form> */}
+      </div>
 
-          <form>
-            <div className="mb-3">
-              <label htmlFor="Name1" className="form-label">
-                Name
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="Name1"
-                onBlur={handleChange}
-                defaultValue={name}
-                aria-describedby="nameHelp"
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="Email1" className="form-label">
-                Email address
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="Email1"
-                onBlur={handleChange}
-                defaultValue={email}
-                aria-describedby="emailHelp"
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="Message1" className="form-label">
-                Message
-              </label>
-              <textarea
-                type="text"
-                className="form-control"
-                id="Message1"
-                onBlur={handleChange}
-                defaultValue={message}
-              />
-            </div>
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </form>
-        </div>
+      <div className="form-wrap">
+        <h1 className="c-title"> Let's Work Together</h1>
+        <p className="c-desc">
+          <b> Leave a message? </b> Get in touch. Always interested if the right
+          project comes along!
+        </p>
+        <form>
+          <div className="mb-3">
+            <label htmlFor="Name1" className="form-label">
+              Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="Name1"
+              onBlur={handleChange}
+              defaultValue={name}
+              aria-describedby="nameHelp"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="Email1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="Email1"
+              onBlur={handleChange}
+              defaultValue={email}
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="Message1" className="form-label">
+              Message
+            </label>
+            <textarea
+              type="text"
+              className="form-control"
+              id="Message1"
+              onBlur={handleChange}
+              defaultValue={message}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
